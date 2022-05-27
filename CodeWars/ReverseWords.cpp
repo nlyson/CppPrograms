eb18f3aba5a9e0ed5479f8e412d30adb89d48338
nlyson <72270566+nlyson@users.noreply.github.com>
2022-05-27 08:40:58 -0500

    Create ReverseWords.cpp

---
+++ CodeWars/ReverseWords.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+ReverseWords.cpp
+Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*******************************************************************************/
+
+std::string reverse_words(std::string str)
+{
+   // Create a stringstream to store the completed reversed string
+   std::stringstream reverseStr;
+  
+   for(std::string::iterator it=str.begin(); it < str.end(); ++it)
+   {
+      // Add characters to the beginning of a temp string until you get to a space
+      std::string reverseWord;
+      while(it < str.end() && *it != ' ')
+      {
+         reverseWord.insert(reverseWord.begin(), *it);
+         it++;
+      }
+     
+      // We found a space.  Put this word in our stringstream and move on to the next word
+      reverseStr << reverseWord << " ";
+   }
+   return reverseStr.str().substr(0, str.size());
+}
